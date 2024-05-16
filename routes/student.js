1b75568f658a8bc32d5c0e6063b7611d1c486e17
= <intahaltallat79@gmail.com>
2024-05-16 16:19:40 +0500

    new branch commit

--- routes/student.js
+++ routes/student.js
@@ -1,9 +1,17 @@
 var express=require("express");
 var router= express.Router();
+var Student = require('../models/student');
+
 //GET Routes
 router.get('/',function(req,res,next){
     res.send("Student Dashboard");
 });
+
+//Get student enrolled in particular course
+router.post('/getStudentEnrolled', function(req, res, next){
+
+});
+
 
 
 
